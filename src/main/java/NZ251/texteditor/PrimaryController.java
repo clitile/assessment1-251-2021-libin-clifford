ba2231ef462f7725a43c3115f7b35095ea3dd799
clitile <xiangwhao@163.com>
2021-09-14 21:04:55 +0800

    add the functions and para names

--- src/main/java/NZ251/texteditor/PrimaryController.java
+++ src/main/java/NZ251/texteditor/PrimaryController.java
@@ -1,12 +1,97 @@
 package NZ251.texteditor;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private MenuItem newF;
+
+    @FXML
+    private MenuItem openF;
+
+    @FXML
+    private MenuItem saveF;
+
+    @FXML
+    private MenuItem pringtB;
+
+    @FXML
+    private MenuItem o2pdf;
+
+    @FXML
+    private MenuItem searchB;
+
+    @FXML
+    private MenuItem copyB;
+
+    @FXML
+    private MenuItem pasteB;
+
+    @FXML
+    private MenuItem cutB;
+
+    @FXML
+    private MenuItem tdB;
+
+    @FXML
+    private MenuItem aboutB;
+
+    @FXML
+    void aboutF(ActionEvent event) {
+
     }
+
+    @FXML
+    void copyF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cutF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pasteF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pdfB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pringtB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tdF(ActionEvent event) {
+
+    }
+
 }
