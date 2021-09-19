67e231d6be9405e4d12462b732249e18dd5ced43
clitile <xiangwhao@163.com>
2021-09-19 10:43:00 +0800

    add the function which can convert texts to pdf

--- src/main/java/NZ251/texteditor/FileOp.java
+++ src/main/java/NZ251/texteditor/FileOp.java
@@ -1,8 +1,18 @@
 package NZ251.texteditor;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import javafx.scene.text.Font;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class FileOp {
+    public static boolean changeFlag = false;
+
     public static String getFileExtension (File file) {
         String extention = "";
         if (file != null && file.exists()) {
@@ -24,6 +34,15 @@ public class FileOp {
     }
 
     public static void printFile(String path) {
+        //TODO
+    }
 
+    public static void O2PDF (String content, File outfile, Font font) throws IOException, DocumentException {
+        Document document = new Document();
+        OutputStream os = new FileOutputStream(outfile);
+        PdfWriter.getInstance(document, os);
+        document.open();
+        document.add(new Paragraph(content, FontFactory.getFont(String.valueOf(font))));
+        document.close();
     }
 }
