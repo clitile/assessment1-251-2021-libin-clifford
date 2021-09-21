9d273df301a4afa9ff6af454df43ea6b7396733a
clitile <xiangwhao@163.com>
2021-09-21 10:19:47 +0800

    add requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module NZ.texteditor {
     requires javafx.controls;
     requires javafx.fxml;
     requires itextpdf;
+    requires java.desktop;
+    requires org.fxmisc.richtext;
+    requires reactfx;
 
     opens NZ251.texteditor to javafx.fxml;
     exports NZ251.texteditor;
