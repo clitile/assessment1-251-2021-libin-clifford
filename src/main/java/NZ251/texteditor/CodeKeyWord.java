9355a84bbbd2062ddf0c0c7478bee6e41d60f799
clitile <xiangwhao@163.com>
2021-09-22 10:01:59 +0800

    manage the key word

---
+++ src/main/java/NZ251/texteditor/CodeKeyWord.java
@@ -0,0 +1,25 @@
+package NZ251.texteditor;
+
+import java.util.regex.Pattern;
+
+public class CodeKeyWord {
+
+    public static final String KEYWORD_PATTERN = "\\b(" + String.join("|", SecondaryController.KEYWORDS) + ")\\b";
+    public static final String PAREN_PATTERN = "\\(|\\)";
+    public static final String BRACE_PATTERN = "\\{|\\}";
+    public static final String BRACKET_PATTERN = "\\[|\\]";
+    public static final String SEMICOLON_PATTERN = "\\;";
+    public static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    public static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/"   // for whole text processing (text blocks)
+            + "|" + "/\\*[^\\v]*" + "|" + "^\\h*\\*([^\\v]*|/)";  // for visible paragraph processing (line by line)
+
+    public static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+}
